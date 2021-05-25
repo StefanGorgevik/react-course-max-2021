c3615e7e5d60556224c3d323005b71ad431154b1
Stefan . Gjorgjevikj <stefan.gjorgjevikj@musala.com>
2021-05-25 14:10:24 +0200

    finished with advanced part, refs, fragments, context, hooks, reducer

---
+++ part2/src/store/reducers/authReducers.js
@@ -0,0 +1,34 @@
+
+export const emailReducer = (state, action) => {
+    switch (action.type) {
+      case "USER_INPUT":
+        return {
+          value: action.val,
+          isValid: action.val.includes("@"),
+        };
+      case "INPUT_BLUR":
+        return {
+          value: state.value,
+          isValid: state.value.includes("@"),
+        };
+      default:
+        return { value: "", isValid: false };
+    }
+  };
+  
+ export const passwordReducer = (state, action) => {
+    switch (action.type) {
+      case "USER_INPUT":
+        return {
+          value: action.val,
+          isValid: action.val.trim().length > 6,
+        };
+      case "INPUT_BLUR":
+        return {
+          value: state.value,
+          isValid: state.value.trim().length > 6,
+        };
+      default:
+        return { value: "", isValid: false };
+    }
+  };
