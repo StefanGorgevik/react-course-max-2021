9fdd329157bd4d35b70874c9822a2daa4c479c57
Stefan . Gjorgjevikj <stefan.gjorgjevikj@musala.com>
2021-05-26 12:03:09 +0200

    finished section for food order app

--- food-order/src/components/Layout/Header.js
+++ food-order/src/components/Layout/Header.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from "react";
 import classes from "./Header.module.css";
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return <Fragment>
       <header className={classes.header}>
           <h1>ReactMeals</h1>
-          <button>Cart</button>
+          <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div>
-        <img />
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious food!"/>
       </div>
   </Fragment>;
 };
